dcb961dc209f18389743ba8bccf80375fcd84d71
Vitalis Ian <viterlis254@gmail.com>
2024-08-31 09:48:17 +0300

    initial

--- lib/dog.py
+++ lib/dog.py
@@ -5,3 +5,14 @@ class Dog:
 
     #Instance method definition
     pass
+class Dog:
+    """Class representing a dog."""
+
+    def bark(self):
+        """Instance method that prints 'Woof!'."""
+        print("Woof!")
+
+    def sit(self):
+        """Instance method that prints 'The dog is sitting.'."""
+        print("The dog is sitting.")
+
--- lib/person.py
+++ lib/person.py
@@ -5,3 +5,13 @@ class Person:
 
     #Instance method definition
     pass
+class Person:
+    """Class representing a person."""
+    
+    def talk(self):
+        """Instance method that prints 'Hello World!'."""
+        print("Hello World!")
+
+    def walk(self):
+        """Instance method that prints 'The person is walking.'."""
+        print("The person is walking.")
